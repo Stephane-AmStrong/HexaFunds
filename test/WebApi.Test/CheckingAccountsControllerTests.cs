1badfd3661c1e5224fec59292d020b3b70e159d3
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-06-11 02:01:25 +0200

    Mise en place des tests sur les contrôleurs

---
+++ test/WebApi.Test/CheckingAccountsControllerTests.cs
@@ -0,0 +1,115 @@
+using DataTransfertObjects;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using Services.Abstractions;
+
+using WebApplicationDocker.Controllers;
+
+namespace WebApi.Test;
+
+public class CheckingAccountsControllerTests
+{
+    private readonly Mock<IServiceManager> _mockServiceManager;
+    private readonly Mock<ICheckingAccountService> _mockCheckingAccountService;
+    private readonly CheckingAccountsController _controller;
+    private readonly CancellationToken _cancellationToken;
+
+    private readonly CheckingAccountRequest _checkingAccountRequest;
+    private readonly CheckingAccountResponse _checkingAccountResponse;
+
+    public CheckingAccountsControllerTests()
+    {
+        _cancellationToken = new CancellationToken();
+        _mockServiceManager = new Mock<IServiceManager>();
+        _mockCheckingAccountService = new Mock<ICheckingAccountService>();
+        _mockServiceManager.Setup(sm => sm.CheckingAccountService).Returns(_mockCheckingAccountService.Object);
+        _controller = new CheckingAccountsController(_mockServiceManager.Object);
+
+        _checkingAccountRequest = new CheckingAccountRequest { AccountNumber = "12345", OverdraftLimit = 500 };
+        _checkingAccountResponse = new CheckingAccountResponse { Id = Guid.NewGuid(), AccountNumber = "12345", OverdraftLimit = 500 };
+    }
+
+    [Fact]
+    public async Task Get_ShouldReturnOkWithCheckingAccounts()
+    {
+        // Arrange
+        var checkingAccounts = new List<CheckingAccountResponse> { _checkingAccountResponse };
+        _mockCheckingAccountService.Setup(s => s.GetAllAsync(_cancellationToken)).ReturnsAsync(checkingAccounts);
+
+        // Act
+        var result = await _controller.Get(CancellationToken.None);
+
+        // Assert
+        _mockCheckingAccountService.Verify(r => r.GetAllAsync(_cancellationToken), Times.Once);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<CheckingAccountResponse>>(okResult.Value);
+        Assert.Equal(checkingAccounts.Count, returnValue.Count);
+    }
+
+    [Fact]
+    public async Task Get_ById_ShouldReturnOkWithCheckingAccount()
+    {
+        // Arrange        
+        _mockCheckingAccountService.Setup(s => s.GetByIdAsync(_checkingAccountResponse.Id, _cancellationToken)).ReturnsAsync(_checkingAccountResponse);
+
+        // Act
+        var result = await _controller.Get(_checkingAccountResponse.Id, CancellationToken.None);
+
+        // Assert
+        _mockCheckingAccountService.Verify(r => r.GetByIdAsync(It.IsAny<Guid>(), _cancellationToken), Times.Once);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<CheckingAccountResponse>(okResult.Value);
+        Assert.Equal(_checkingAccountResponse.Id, returnValue.Id);
+        Assert.Equal(_checkingAccountResponse.AccountNumber, returnValue.AccountNumber);
+    }
+
+    [Fact]
+    public async Task Post_ShouldReturnCreatedAtActionWithCheckingAccount()
+    {
+        // Arrange
+        _mockCheckingAccountService.Setup(s => s.CreateAsync(_checkingAccountRequest, _cancellationToken)).ReturnsAsync(_checkingAccountResponse);
+
+        // Act
+        var result = await _controller.Post(_checkingAccountRequest, CancellationToken.None);
+
+        // Assert
+        _mockCheckingAccountService.Verify(r => r.CreateAsync(It.IsAny<CheckingAccountRequest>(), _cancellationToken), Times.Once);
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+        var returnValue = Assert.IsType<CheckingAccountResponse>(createdAtActionResult.Value);
+        Assert.Equal(_checkingAccountResponse.Id, returnValue.Id);
+    }
+
+    [Fact]
+    public async Task Delete_ShouldReturnNoContent()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        _mockCheckingAccountService.Setup(s => s.DeleteAsync(id, _cancellationToken)).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.Delete(id, CancellationToken.None);
+
+        // Assert
+        _mockCheckingAccountService.Verify(r => r.DeleteAsync(It.IsAny<Guid>(), _cancellationToken), Times.Once);
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public async Task Put_ShouldReturnNoContent()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+       
+        _mockCheckingAccountService.Setup(s => s.UpdateAsync(id, _checkingAccountRequest, _cancellationToken)).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.Put(id, _checkingAccountRequest, CancellationToken.None);
+
+        // Assert
+        _mockCheckingAccountService.Verify(r => r.UpdateAsync(It.IsAny<Guid>(), It.IsAny<CheckingAccountRequest>(), _cancellationToken), Times.Once);
+        Assert.IsType<NoContentResult>(result);
+    }
+}
---
+++ test/WebApi.Test/SavingsAccountsControllerTests.cs
@@ -0,0 +1,115 @@
+using DataTransfertObjects;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using Services.Abstractions;
+
+using WebApplicationDocker.Controllers;
+
+namespace WebApi.Test;
+
+public class SavingsAccountsControllerTests
+{
+    private readonly Mock<IServiceManager> _mockServiceManager;
+    private readonly Mock<ISavingsAccountService> _mockSavingsAccountService;
+    private readonly SavingsAccountsController _controller;
+    private readonly CancellationToken _cancellationToken;
+
+    private readonly SavingsAccountRequest _savingsAccountRequest;
+    private readonly SavingsAccountResponse _savingsAccountResponse;
+
+    public SavingsAccountsControllerTests()
+    {
+        _cancellationToken = new CancellationToken();
+        _mockServiceManager = new Mock<IServiceManager>();
+        _mockSavingsAccountService = new Mock<ISavingsAccountService>();
+        _mockServiceManager.Setup(sm => sm.SavingsAccountService).Returns(_mockSavingsAccountService.Object);
+        _controller = new SavingsAccountsController(_mockServiceManager.Object);
+
+        _savingsAccountRequest = new SavingsAccountRequest { AccountNumber = "12345", BalanceCeiling = 500 };
+        _savingsAccountResponse = new SavingsAccountResponse { Id = Guid.NewGuid(), AccountNumber = "12345", BalanceCeiling = 500 };
+    }
+
+    [Fact]
+    public async Task Get_ShouldReturnOkWithSavingsAccounts()
+    {
+        // Arrange
+        var savingsAccounts = new List<SavingsAccountResponse> { _savingsAccountResponse };
+        _mockSavingsAccountService.Setup(s => s.GetAllAsync(_cancellationToken)).ReturnsAsync(savingsAccounts);
+
+        // Act
+        var result = await _controller.Get(CancellationToken.None);
+
+        // Assert
+        _mockSavingsAccountService.Verify(r => r.GetAllAsync(_cancellationToken), Times.Once);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<List<SavingsAccountResponse>>(okResult.Value);
+        Assert.Equal(savingsAccounts.Count, returnValue.Count);
+    }
+
+    [Fact]
+    public async Task Get_ById_ShouldReturnOkWithSavingsAccount()
+    {
+        // Arrange        
+        _mockSavingsAccountService.Setup(s => s.GetByIdAsync(_savingsAccountResponse.Id, _cancellationToken)).ReturnsAsync(_savingsAccountResponse);
+
+        // Act
+        var result = await _controller.Get(_savingsAccountResponse.Id, CancellationToken.None);
+
+        // Assert
+        _mockSavingsAccountService.Verify(r => r.GetByIdAsync(It.IsAny<Guid>(), _cancellationToken), Times.Once);
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        var returnValue = Assert.IsType<SavingsAccountResponse>(okResult.Value);
+        Assert.Equal(_savingsAccountResponse.Id, returnValue.Id);
+        Assert.Equal(_savingsAccountResponse.AccountNumber, returnValue.AccountNumber);
+    }
+
+    [Fact]
+    public async Task Post_ShouldReturnCreatedAtActionWithSavingsAccount()
+    {
+        // Arrange
+        _mockSavingsAccountService.Setup(s => s.CreateAsync(_savingsAccountRequest, _cancellationToken)).ReturnsAsync(_savingsAccountResponse);
+
+        // Act
+        var result = await _controller.Post(_savingsAccountRequest, CancellationToken.None);
+
+        // Assert
+        _mockSavingsAccountService.Verify(r => r.CreateAsync(It.IsAny<SavingsAccountRequest>(), _cancellationToken), Times.Once);
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+        var returnValue = Assert.IsType<SavingsAccountResponse>(createdAtActionResult.Value);
+        Assert.Equal(_savingsAccountResponse.Id, returnValue.Id);
+    }
+
+    [Fact]
+    public async Task Delete_ShouldReturnNoContent()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        _mockSavingsAccountService.Setup(s => s.DeleteAsync(id, _cancellationToken)).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.Delete(id, CancellationToken.None);
+
+        // Assert
+        _mockSavingsAccountService.Verify(r => r.DeleteAsync(It.IsAny<Guid>(), _cancellationToken), Times.Once);
+        Assert.IsType<NoContentResult>(result);
+    }
+
+    [Fact]
+    public async Task Put_ShouldReturnNoContent()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        _mockSavingsAccountService.Setup(s => s.UpdateAsync(id, _savingsAccountRequest, _cancellationToken)).Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _controller.Put(id, _savingsAccountRequest, CancellationToken.None);
+
+        // Assert
+        _mockSavingsAccountService.Verify(r => r.UpdateAsync(It.IsAny<Guid>(), It.IsAny<SavingsAccountRequest>(), _cancellationToken), Times.Once);
+        Assert.IsType<NoContentResult>(result);
+    }
+}
