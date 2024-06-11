3fdb8f6c734d6bda228c229385035bd218053068
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-06-11 02:00:47 +0200

    Mise en place de Persistence

---
+++ src/Infrastructure/Persistence/Repository/SavingsAccountRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Repositories.Abstractions;
+
+using Microsoft.EntityFrameworkCore;
+namespace Persistence.Repository;
+
+public sealed class SavingsAccountRepository(BankingDbContext dbContext) : RepositoryBase<SavingsAccount>(dbContext), ISavingsAccountRepository
+{
+    public async Task CreateAsync(SavingsAccount savingsAccount, CancellationToken cancellationToken)
+    {
+        await BaseCreateAsync(savingsAccount, cancellationToken);
+    }
+
+    public async Task DeleteAsync(SavingsAccount savingsAccount, CancellationToken cancellationToken)
+    {
+        await Task.Run(()=> BaseDelete(savingsAccount), cancellationToken);
+    }
+
+    public async Task<IEnumerable<SavingsAccount>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await BaseGetAll().ToListAsync(cancellationToken);
+    }
+
+    public async Task<SavingsAccount?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await BaseFindByCondition(checkingAccount => checkingAccount.Id.Equals(id))
+            .Include(x => x.Transactions)
+            .FirstOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task UpdateAsync(SavingsAccount savingsAccount, CancellationToken cancellationToken)
+    {
+        await Task.Run(()=> BaseUpdate(savingsAccount), cancellationToken);
+    }
+}
