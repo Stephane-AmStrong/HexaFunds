926bd556e64f205d4b522aa4c13467960c3b0456
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-07-19 12:02:30 +0200

    Elimination de code mort

--- src/Core/DataTransfertObjects/TransactionResponse.cs
+++ src/Core/DataTransfertObjects/TransactionResponse.cs
@@ -4,5 +4,4 @@ public record TransactionResponse : TransactionRequest
 {
     public Guid Id { get; init; }
     public DateTime Date { get; init; }
-    public virtual required BankAccountResponse BankAccount { get; init; }
 }
