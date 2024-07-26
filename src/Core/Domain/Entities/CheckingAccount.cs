b7b145cb05403e796e3a31757825e0d34feebfec
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-07-26 14:23:40 +0200

    refactor(entities): changer les records en classes

--- src/Core/Domain/Entities/CheckingAccount.cs
+++ src/Core/Domain/Entities/CheckingAccount.cs
@@ -1,6 +1,6 @@
 ﻿namespace Domain.Entities;
 
-public record CheckingAccount : BankAccount
+public class CheckingAccount : BankAccount
 {
     public float OverdraftLimit { get; init; }
 }
--- src/Core/Domain/Entities/Transaction.cs
+++ src/Core/Domain/Entities/Transaction.cs
@@ -4,15 +4,15 @@ using BankAccount.Core.Enumerations;
 
 namespace Domain.Entities;
 
-public record Transaction
+public class Transaction
 {
     public Guid Id { get; init; }
     public float Amount { get; init; }
-    public TransactionType Type { get; set; }
-    public DateTime Date { get; set; }
+    public TransactionType Type { get; init; }
+    public DateTime Date { get; init; }
     public Guid AccountId { get; init; }
 
     [ForeignKey("AccountId")]
-    public virtual required BankAccount BankAccount { get; set; }
+    public virtual required BankAccount BankAccount { get; init; }
 };
 
