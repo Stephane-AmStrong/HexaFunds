d7fc883a76beac84122f2d9d825869130d363158
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-08-08 17:47:30 +0200

    refactor(DTO): serialiser les enum

--- src/Core/DataTransfertObjects/TransactionRequest.cs
+++ src/Core/DataTransfertObjects/TransactionRequest.cs
@@ -5,6 +5,7 @@ namespace DataTransfertObjects;
 public record TransactionRequest
 {
     public float Amount { get; init; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public TransactionType Type { get; init; }
     public Guid AccountId { get; init; }
     [JsonIgnore]
