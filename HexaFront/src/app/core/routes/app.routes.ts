8669b978d86bdb514c3d6b80994bfdb5fc9c7a4f
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-09-18 11:59:19 +0200

    feat(frontend): update layout

--- HexaFront/src/app/core/routes/app.routes.ts
+++ HexaFront/src/app/core/routes/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { AddressFormComponent } from '../../features/banking/Components/address-form/address-form.component';
 import { DashboardComponent } from '../../features/banking/Components/dashboard/dashboard.component';
-import { DragDropComponent } from '../../features/banking/Components/drag-drop/drag-drop.component';
 import { PageNotFoundComponent } from '../../shared/Components/page-not-found/page-not-found.component';
-import { TreeComponent } from '../../features/banking/Components/tree/tree.component';
 
 export const routes: Routes = [
     {
@@ -18,14 +16,6 @@ export const routes: Routes = [
     {
         path: 'address-form',
         component: AddressFormComponent,
-    },
-    {
-        path: 'tree',
-        component: TreeComponent,
-    },
-    {
-        path: 'drag-drop',
-        component: DragDropComponent,
     },
     {
         path:'**',
