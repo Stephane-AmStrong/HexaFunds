8e6288f5075203a93e8b1521b7b561c4b697aaa7
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-09-19 18:53:02 +0200

    feat(front): add crud implementation to checking and savings accounts service

--- HexaFront/src/app/features/banking/services/checking-account.service.ts
+++ HexaFront/src/app/features/banking/services/checking-account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable, shareReplay } from 'rxjs';
 import { CheckingAccountResponse } from '../models/checking-account-response';
+import { CheckingAccountRequest } from '../models/checking-account-request';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,29 @@ export class CheckingAccountService {
     return this.http
       .get<CheckingAccountResponse[]>(this.BASE_URL)
       .pipe(shareReplay());
+  }
+
+  getById(id: string): Observable<CheckingAccountResponse> {
+    return this.http
+      .get<CheckingAccountResponse>(`${this.BASE_URL}/${id}`)
+      .pipe(shareReplay());
+  }
+
+  create(
+    checkingAccount: CheckingAccountRequest
+  ): Observable<CheckingAccountResponse> {
+    return this.http
+      .post<CheckingAccountResponse>(this.BASE_URL, checkingAccount)
+      .pipe(shareReplay());
+  }
+
+  update(id: string, changes: Partial<CheckingAccountRequest>) {
+    return this.http
+      .put<CheckingAccountResponse>(`${this.BASE_URL}/${id}`, changes)
+      .pipe(shareReplay());
+  }
+
+  delete(id: string) {
+    return this.http.delete(`${this.BASE_URL}/${id}`).pipe(shareReplay());
   }
 }
