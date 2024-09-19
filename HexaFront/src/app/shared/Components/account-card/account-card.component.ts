8dbc1568e64ba2bd954eccb85bc72e5602584bd6
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-09-19 19:00:45 +0200

    feat(front): style card component

--- HexaFront/src/app/shared/Components/account-card/account-card.component.ts
+++ HexaFront/src/app/shared/Components/account-card/account-card.component.ts
@@ -1,17 +1,43 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { CheckingAccountResponse } from '../../../features/banking/models/checking-account-response';
+import { SavingsAccountResponse } from '../../../features/banking/models/savings-account-response';
 
 @Component({
   selector: 'account-card',
   standalone: true,
-  imports: [MatMenuModule, MatIconModule, MatButtonModule, MatCardModule, MatRippleModule],
+  imports: [
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatRippleModule,
+  ],
   templateUrl: './account-card.component.html',
   styleUrl: './account-card.component.scss',
 })
 export class AccountCardComponent {
-  cardTitle = input.required<string>()
+  bankAccount = input.required<
+    CheckingAccountResponse | SavingsAccountResponse
+  >();
+
+  checkingAccount = computed(() => {
+    return (this.bankAccount() as CheckingAccountResponse).overdraftLimit !== undefined
+      ? (this.bankAccount() as CheckingAccountResponse)
+      : null;
+  });
+
+  savingsAccount = computed(() => {
+    return (this.bankAccount() as SavingsAccountResponse).balanceCeiling !== undefined
+      ? (this.bankAccount() as SavingsAccountResponse)
+      : null;
+  });
+
+  balancePercentage = computed(() => {
+    return Math.min(100, (this.bankAccount().balance / (this.checkingAccount() ? this.checkingAccount()?.overdraftLimit! : this.savingsAccount()?.balanceCeiling!)) * 100);
+  });
 }
