1c9afea049da2295577df195d675081ae38238ad
StephaneAmStrong <stephane.adjakotan@gmail.com>
2024-09-20 19:01:48 +0200

    feat(front): implement transaction resolver

--- HexaFront/src/app/features/banking/resolvers/checking-account.resolver.spec.ts
+++ HexaFront/src/app/features/banking/resolvers/checking-account.resolver.spec.ts
@@ -2,9 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { ResolveFn } from '@angular/router';
 
 import { checkingAccountResolver } from './checking-account.resolver';
+import { CheckingAccountResponse } from '../models/checking-account-response';
 
 describe('checkingAccountResolver', () => {
-  const executeResolver: ResolveFn<boolean> = (...resolverParameters) => 
+  const executeResolver: ResolveFn<CheckingAccountResponse> = (...resolverParameters) => 
       TestBed.runInInjectionContext(() => checkingAccountResolver(...resolverParameters));
 
   beforeEach(() => {
