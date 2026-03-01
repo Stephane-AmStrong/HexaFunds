774f3df28d3ee9cf6d69ed4e686027730e055605
Stephane AmStrong <stephane.adjakotan@gmail.com>
2026-03-01 13:50:01 +0100

    Configure TUnit

---
+++ HexaBack/test/Application.Test/ExampleTests.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Application.Test;
+
+public class ExampleTests
+{
+
+    [Test]
+    public async Task ExempleTest()
+    {
+        var sum = Add(3,2);
+
+        await Assert.That(sum).IsEqualTo(5);
+    }
+
+    private static int Add(int x, int y) => x+y;
+}
